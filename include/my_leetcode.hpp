2d778f6b0bb8542f23df88641202b368767e8c6a
lijiancong <18221336269@163.com>
2020-07-23 20:52:49 +0800

    首个版本

---
+++ include/my_leetcode.hpp
@@ -0,0 +1,41 @@
+///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+/// Copyright (c) 2019,2020 Lijiancong. All rights reserved.
+///
+/// Use of this source code is governed by a MIT license
+/// that can be found in the License file.
+///
+/// @file   my_leetcode.hpp
+/// @brief  我的leetcode题解
+///
+/// @author lijiancong, pipinstall@163.com
+/// @date   2020-07-23 20:13:41
+///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+
+#ifndef INCLUDE_MY_LEETCODE_HPP_
+#define INCLUDE_MY_LEETCODE_HPP_
+
+#include <vector>
+
+namespace lee {
+inline namespace leetcode {
+
+/// 15. 三数之和
+/// 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c
+/// ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+///
+/// 注意：答案中不可以包含重复的三元组。
+///
+/// 示例：
+///
+/// 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+///
+/// 满足要求的三元组集合为：
+/// [
+///   [-1, 0, 1],
+///   [-1, -1, 2]
+/// ]
+std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {}
+}  // namespace leetcode
+}  // namespace lee
+
+#endif  // INCLUDE_MY_LEETCODE_HPP_
